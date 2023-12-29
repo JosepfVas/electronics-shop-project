374efb9aca15aa15c41fa9e379a69fb9c6b30094
Josef Vas <yovzhi@icloud.com>
2023-12-30 00:00:51 +0100

    adding homework 1 + tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,21 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+
+@pytest.fixture
+def test_item():
+    return Item(name="Test Item", price=10.0, quantity=2)
+def test_calculate_total_price(test_item):
+    assert test_item.calculate_total_price() == 20.0
+
+def test_apply_discount(test_item):
+    test_item.apply_discount()
+    assert test_item.price == 10.0 * test_item.pay_rate
+def test_all_items_list(test_item):
+    assert test_item in Item.all
+def test_item_attributes(test_item):
+    assert test_item.name == "Test Item"
+    assert test_item.price == 10.0
+    assert test_item.quantity == 2
+
+
